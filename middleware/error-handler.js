6bc18dffc6a7065c21d0d63014b95cf4544e3392
HitomiHuang <black011602@gmail.com>
2023-07-14 11:51:11 +0800

    add error-handler

---
+++ middleware/error-handler.js
@@ -0,0 +1,38 @@
+const { NotFoundException, AuthErrorException, InputErrorException, UploadFailedException, EmailOrPasswordWrongException } = require('../util/exceptions')
+const httpStatusCodes = require('../util/httpStatusCodes')
+module.exports = {
+  apiErrorHandler (err, req, res, next) {
+    if (err instanceof NotFoundException) {
+      res.status(httpStatusCodes.NOT_FOUND).json({
+        status: `${err.name}`,
+        message: `${err.message}`
+      })
+    } else if (err instanceof AuthErrorException) {
+      res.status(httpStatusCodes.AUTH_ERROR).json({
+        status: `${err.name}`,
+        message: `${err.message}`
+      })
+    } else if (err instanceof UploadFailedException) {
+      res.status(httpStatusCodes.UPLOAD_FAILED).json({
+        status: `${err.name}`,
+        message: `${err.message}`
+      })
+    } else if (err instanceof InputErrorException) {
+      res.status(httpStatusCodes.BAD_REQUEST).json({
+        status: `${err.name}`,
+        message: `${err.message}`
+      })
+    } else if (err instanceof EmailOrPasswordWrongException) {
+      res.status(httpStatusCodes.BAD_REQUEST).json({
+        status: `${err.name}`,
+        message: `${err.message}`
+      })
+    } else {
+      res.status(httpStatusCodes.SERVER_ERROR).json({
+        status: 'System Error',
+        message: `${err.message}`
+      })
+    }
+    next(err)
+  }
+}
---
+++ utils/httpStatusCodes.js
@@ -0,0 +1,10 @@
+const httpStatusCodes = {
+  OK: 200,
+  BAD_REQUEST: 400,
+  NOT_FOUND: 404,
+  AUTH_ERROR: 401,
+  UPLOAD_FAILED: 504,
+  SERVER_ERROR: 500
+}
+
+module.exports = httpStatusCodes
