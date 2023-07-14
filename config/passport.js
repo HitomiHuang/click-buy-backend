4bbbd25f9f3fbe82ac18640f61824f1e2d3374c1
HitomiHuang <black011602@gmail.com>
2023-07-14 08:37:31 +0800

    add passport and exceptions

---
+++ config/passport.js
@@ -0,0 +1,39 @@
+const passport = require('passport')
+const LocalStrategy = require('passport-local')
+const passportJWT = require('passport-jwt')
+const { NotFoundException, EmailOrPasswordWrongException } = require('../util/exceptions')
+const { User } = require('../models')
+const JWTSECRET = process.env.JWT_SECRET
+
+const JWTStrategy = passportJWT.Strategy
+const ExtractJWT = passportJWT.ExtractJwt
+
+passport.use(new LocalStrategy(
+  {
+    usernameField: 'account',
+    passwordField: 'password'
+  },
+  async (account, password, done) => {
+    try {
+      const user = await User.findOne({ where: { account } })
+      if (!user) throw new NotFoundException('the email have not been registered')
+      if (user.password !== password) throw new EmailOrPasswordWrongException('email or password wrong')
+      return done(null, user)
+    } catch (err) {
+      return done(err, false)
+    }
+  }
+))
+
+const jwtOptions = {
+  jwtFromRequest: ExtractJWT.fromAuthHeaderAsBearerToken(),
+  secretOrKey: JWTSECRET
+}
+
+passport.use(new JWTStrategy(jwtOptions, (jwtPayload, cb) => {
+  User.findByPk(jwtPayload.id)
+    .then(user => cb(null, user))
+    .catch(err => cb(err))
+}))
+
+module.exports = passport
